a027e34b3d82837da521fdedb8b1a7052b801ca2
zhuojh <975786355@qq.com>
2017-06-19 10:01:20 +0800

    add two numbers solution

--- acm/src/com/cge/acm/AddTwoNumbers.java
+++ acm/src/com/cge/acm/AddTwoNumbers.java
@@ -1,41 +1,61 @@
 package com.cge.acm;
 
+/**
+ * 
+ * @author zhuojh
+ * {@link https://leetcode.com/problems/add-two-numbers/#/description}
+ *
+ */
 public class AddTwoNumbers {
 
 	public static void main(String[] args) {
 		
-	}
-	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       ListNode node = null;
-		int mod = 0;
-		if(l1.val+l2.val<10){
-			node = new ListNode(l1.val+l2.val);
-		} else {
-			node = new ListNode((l1.val+l2.val)%10);
-			mod=1;
-		}
-		ListNode firstNote = node;
-		l1 = l1.next;
-		l2 = l2.next;
-		while(l1!=null && l2!=null){
-			if((l1.val + l2.val)<10){
-				ListNode childNode = new ListNode(l1.val+l2.val+mod);
-				node.next = childNode;
-				mod=0;
-			} else {
-				ListNode childNode = new ListNode((l1.val+l2.val)%10+mod);
-				node.next = childNode;
-				mod = 1;
-			}
-			l1 = l1.next;
-			l2 = l2.next;
+		ListNode l1 = new ListNode(2);
+		ListNode l11 = new ListNode(4);
+		ListNode l12 = new ListNode(3);
+		l1.next = l11;
+		l11.next = l12;
+		
+		ListNode l2 = new ListNode(5);
+		ListNode l21 = new ListNode(6);
+		ListNode l22 = new ListNode(4);
+		l2.next = l21;
+		l21.next = l22;
+		
+		ListNode node = new AddTwoNumbers().addTwoNumbers(l1,l2);
+		while(node!=null){
+			System.out.print(node.val);
 			node = node.next;
+			if(node!=null) System.out.print(",");
 		}
-		return firstNote;
-    }
-}
-	 class ListNode {
-	      int val;
-	      ListNode next;
-	      ListNode(int x) { val = x; next=null;}
+		
 	}
+	
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2){
+		ListNode node = new ListNode(0);
+		ListNode firstNode = node;
+		int sum = 0;
+		while(l1 != null || l2 != null){
+			sum /=10;
+			if(l1 != null){
+				sum+=l1.val;
+				l1 = l1.next;
+			}
+			if(l2 != null){
+				sum+=l2.val;
+				l2 = l2.next;
+			}
+			node.next = new ListNode(sum%10);
+			node = node.next;
+			
+		}
+		if(sum/10==1)
+			node.next = new ListNode(1);
+		return firstNode.next;
+	}
+}
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; next=null;}
+}
