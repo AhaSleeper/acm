28afcbc0c3661bedcd67c7214aae070c31fef19f
zhuojh <975786355@qq.com>
2018-05-18 13:52:09 +0800

    reverse linkedlist

--- acm/src/com/cge/acm/ReverseLinkedList.java
+++ acm/src/com/cge/acm/ReverseLinkedList.java
@@ -22,12 +22,36 @@ public class ReverseLinkedList {
 		 }
 		 
 	 }
+	 
+	 public ListNode iterativeReverse(ListNode head){
+		 ListNode pre = head, next= head.next, temp=null;
+		 pre.next=null;
+		 if(next.next==null) {
+			 next.next = pre;
+			 return next;
+		 }
+		 while(next!=null){
+			temp = next.next;
+			next.next = pre;
+			if(temp.next!=null)
+				pre = next;
+			else {
+				temp.next = next;
+				next.next = pre;
+				return temp;
+			}
+			next = temp.next;
+		 }
+		 return next;
+	 }
 	 public static void main(String[] args) {
 		ListNode node1 = new ListNode(1);
 		ListNode node2 = new ListNode(2);
 		node1.next = node2;
 		ReverseLinkedList reverse = new ReverseLinkedList();
-		ListNode result = reverse.reverseList(node1);
+//		ListNode result = reverse.reverseList(node1);
+		ListNode result = reverse.iterativeReverse(node1);
+		
 		while(result!=null){
 			System.out.println(result.val);
 			result = result.next;
