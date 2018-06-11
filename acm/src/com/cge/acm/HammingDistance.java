39f75dd607c7ed16997765eda2ec6cf313b29af6
zhuojh <975786355@qq.com>
2018-06-11 23:11:13 +0800

    Hamming distance

---
+++ acm/src/com/cge/acm/HammingDistance.java
@@ -0,0 +1,10 @@
+package com.cge.acm;
+
+public class HammingDistance {
+	public static int hammingDistance(int x, int y) {
+		return Integer.bitCount(x ^ y);
+    }
+	public static void main(String[] args) {
+		System.out.println(hammingDistance(1, 4));
+	}
+}
