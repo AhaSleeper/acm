3468ca5cf5c878855c16f0b272a2b137940f5914
zhuojh <975786355@qq.com>
2018-07-17 16:19:11 +0800

    three sum closest

---
+++ acm/src/com/cge/acm/ThreeSumClosest.java
@@ -0,0 +1,30 @@
+package com.cge.acm;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+	
+	public static int threeSumClosest(int[] nums, int target) {
+        int result = nums[0]+nums[1]+nums[nums.length-1];
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-2; i++){
+        	int start = i+1, end = nums.length-1;
+        	while(start<end){
+        		int sum = nums[i] + nums[start]+nums[end];
+        		if(sum > target){
+        			end--;
+        		} else {
+        			start++;
+        		}
+        		if(Math.abs(sum-target) < Math.abs(result-target)){
+        			result = sum;
+        		}
+        	}
+        }
+        return result;
+    }
+	public static void main(String[] args) {
+		int[] nums = {1,1,1,0};
+		System.out.println(threeSumClosest(nums, -100));
+	}
+}
