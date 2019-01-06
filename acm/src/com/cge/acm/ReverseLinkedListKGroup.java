be55559852ad21617f049ef9d4404b0ea7722f32
zhuojh <975786355@qq.com>
2019-01-06 22:36:31 +0800

    modify

--- acm/src/com/cge/acm/ReverseLinkedListKGroup.java
+++ acm/src/com/cge/acm/ReverseLinkedListKGroup.java
@@ -1,11 +1,39 @@
 package com.cge.acm;
 
 public class ReverseLinkedListKGroup {
-	public ListNode reverseKGroup(ListNode head, int k){
-		
-		return head;
+	public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode curr = head;
+        int count = 0;
+        while(curr!=null && count!=k){
+            curr = curr.next;
+            count++;
+        }
+        if(count>0){
+            curr = reverseKGroup(curr, k);
+            while(count-->0){
+                ListNode temp = head.next;
+                head.next = curr;
+                curr = head;
+                head = temp;
+            }
+            head = curr;
+        }
+        return head;
+    }
+	public ListNode reverseList(ListNode head){
+		if(null == head || head.next==null) return head;
+		ListNode temp = head.next;
+		head.next = null;
+		head = temp;
+		return reverse(head, head.next);
 	}
-	public static void main(String[] args) {
-		
+	
+	public ListNode reverse(ListNode pre, ListNode next){
+		if(next==null) return pre;
+		ListNode temp = next.next;
+		next.next = pre;
+		pre = next;
+		next = temp;
+		return reverse(pre, next);
 	}
 }
